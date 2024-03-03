f6814d7eb3ec6e38c5e2b855fb07bd6d6276bb2f
Ahmedelashry <elashryprogramming@gmail.com>
2024-03-03 08:34:49 +0200

    Linked items with cats and members

--- admin/items.php
+++ admin/items.php
@@ -89,6 +89,43 @@ if (isset($_SESSION['Username'])) {
                 </div>
                 <!-- End Item status Field -->
 
+                <!-- Start Members Field -->
+                <div class="form-group form-group-lg">
+                    <label class="col-sm-2 control-label">Members</label>
+                    <div class="col-sm-10 col-md-6">
+                        <select name = "members">
+                            <option value="0">...</option>
+                            <?php
+                                $stmt = $dbconc->prepare("SELECT * FROM users");
+                                $stmt->execute();
+                                $users = $stmt->fetchAll();
+                                foreach ($users as $user){
+                                    echo "<option value='". $user['userID']."'>".$user['userName']."</option>";
+                                }
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                <!-- End Members Field -->
+                <!-- Start categories Field -->
+                <div class="form-group form-group-lg">
+                    <label class="col-sm-2 control-label">Categories</label>
+                    <div class="col-sm-10 col-md-6">
+                        <select name = "categories">
+                            <option value="0">...</option>
+                            <?php
+                                $stmt = $dbconc->prepare("SELECT * FROM categories");
+                                $stmt->execute();
+                                $cats = $stmt->fetchAll();
+                                foreach ($cats as $cat){
+                                    echo "<option value='". $cat['catID']."'>".$cat['catName']."</option>";
+                                }
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                <!-- End categories Field -->
+
                 <!-- Start Submit Field -->
                 <div class="form-group form-group-lg">
                     <div class="col-sm-offset-2 col-sm-10">
@@ -101,7 +138,67 @@ if (isset($_SESSION['Username'])) {
         <?php
 
     } elseif ($do == 'Insert') {
+        //Start Insert page
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            echo "<h1 class='text-center'>Add Item</h1>";
+            echo "<div class='container'>";
+            $Name           = $_POST['Name'];
+            $description    = $_POST['description'];
+            $price          = $_POST['price'];
+            $country        = $_POST['country'];
+            $status         = $_POST['status'];
+            $members        = $_POST['members'];
+            $categories     = $_POST['categories'];
+            // Validate the form
+            $formErrors = array();
+            if (empty($Name)) {
+                $formErrors[] = 'Username can\'t be <strong>empty</strong>';
+            }
+            if (empty($description)) {
+                $formErrors[] = 'Description can\'t be <strong>empty</strong>';
+            }
+            if (empty($price)) {
+                $formErrors[] = 'Price can\'t be <strong>empty</strong>';
+            }
+            if (empty($country)) {
+                $formErrors[] = 'Country can\'t be <strong>empty</strong>';
+            }
+            if ($status === 0) {
+                $formErrors[] = 'You must choose the <strong>Status</strong>';
+            }
+            if ($members === 0) {
+                $formErrors[] = 'You must choose the <strong>member</strong>';
+            }
+            if ($categories === 0) {
+                $formErrors[] = 'You must choose the <strong>categorie</strong>';
+            }
+            foreach ($formErrors as $error) {
+                echo  '<div class="alert alert-danger">' . $error . '</div>';
+            }
+            if (empty($formErrors)) {
+                    $stmt = $dbconc->prepare("INSERT INTO 
+                                            items(Name, Description, Price, AddedDate,Country, status, Cat_ID, Member_ID)
+                                            VALUES(:name, :des, :price, now(), :country, :status, :cat_id, :member_id)");
+                    $stmt->execute(array(
+                        'name'          => $Name,
+                        'des'           => $description,
+                        'price'         => $price,
+                        'country'       => $country,
+                        'status'        => $status,
+                        'cat_id'        => $categories,
+                        'member_id'     => $members,
+                    ));
+                    $msg = "<div class='alert alert-success'>" . $stmt->rowCount() . ' Record Inserted </div>';
+                    redirectHome($msg, 'back');
 
+            }
+        } else {
+            echo "<div class ='container'>";
+            $msg = "<div class ='alert alert-danger'>You are not authorized to view this page.</div>";
+            redirectHome($msg);
+            echo "</div>";
+        }
+        echo "</div>";
 
     } elseif ($do == 'Edit') {
 
